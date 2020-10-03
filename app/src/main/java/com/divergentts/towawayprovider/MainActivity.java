5ed8b5bb74e528c96808d33efc85bd6fdd68f3ca
PETULABM <Chulagp@email.com>
2020-10-03 10:14:24 +0200

    PBB

--- app/src/main/java/com/divergentts/towawayprovider/MainActivity.java
+++ app/src/main/java/com/divergentts/towawayprovider/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        I checked it out and made a chnage
+       v
     }
 }
