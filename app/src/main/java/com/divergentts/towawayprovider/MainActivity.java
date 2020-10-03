023cd36838608a9cbd9df7aec4409b13b1e288ae
PETULABM <Chulagp@email.com>
2020-10-03 15:10:46 +0200

    PBB

--- app/src/main/java/com/divergentts/towawayprovider/MainActivity.java
+++ app/src/main/java/com/divergentts/towawayprovider/MainActivity.java
@@ -3,14 +3,25 @@ package com.divergentts.towawayprovider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void signup(View view) {
+        
+    }
+
+    public void login(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       v
+
+
     }
 }
