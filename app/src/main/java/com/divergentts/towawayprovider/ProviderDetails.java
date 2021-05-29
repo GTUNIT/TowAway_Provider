55431bd96ce98e3a27807e0d028b4ef6039336e6
Mozzibozz <tammyjaneh@gmail.com>
2021-05-29 16:10:44 +0200

    Adding provider screen and welcome buttons

---
+++ app/src/main/java/com/divergentts/towawayprovider/ProviderDetails.java
@@ -0,0 +1,14 @@
+package com.divergentts.towawayprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProviderDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_details);
+    }
+}
--- app/src/main/java/com/divergentts/towawayprovider/Welcome.java
+++ app/src/main/java/com/divergentts/towawayprovider/Welcome.java
@@ -2,12 +2,15 @@ package com.divergentts.towawayprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
     public void begin (View view) {
+        Intent providerdetails = new Intent(this, com.divergentts.towawayprovider.ProviderDetails.class);
+        startActivity(providerdetails);
 
     }
 
